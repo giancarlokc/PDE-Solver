c4f62ad2995caee7faa3a71f67c821297fdd2a62
GIANCARLO KLEMM CAMILO <gkc11@inf.ufpr.br>
2015-04-30 17:06:50 -0300

    Added some argument handling and error handling functions

--- trab1.c
+++ trab1.c
@@ -8,10 +8,44 @@
 //  .  .  .
 //  Xn Yn Zn
 
+// F(x) = 4*(PI)^2 * sin(2*PI*x) * sinh(2*PI*y)
+// Domain: x = [0,2] , y = [0,1]
+
+#define DEBUG 1
+
 #include <stdio.h>
+#include <stdlib.h>
+
+int error(char* error_mesg, int line) {
+    printf("ERROR: %s", error_mesg);
+    if(DEBUG) {
+        printf(" (AT LINE %d)", line);
+    }
+    printf("\n");
+    return -1;
+}
 
 int main(int argc, char **argv) {
+    // Usage
     if(argc < 4) {
         printf("Usage: trab1 [N x] [N y] [Number of iterations]\n");
     }
+    
+    // Get and check parameters
+    double nx = strtod(argv[1], NULL);
+    if(nx == 0.0) {
+        return error("[N x] can NOT be zero", __LINE__);
+    }
+    double ny = strtod(argv[2], NULL);
+    if(ny == 0.0) {
+        return error("[N y] can NOT be zero", __LINE__);
+    }
+    double n_iterations = strtod(argv[3], NULL);
+    if(n_iterations == 0.0) {
+        return error("[Number of iterations] can NOT be zero", __LINE__);
+    }
+    
+    // Calculate hx and hy
+    
+    
 }

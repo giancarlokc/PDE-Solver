26001036130f0c3c9e0b4c1845ce9a793468a832
Giancarlo Klemm Camilo <giancarlokc@gmail.com>
2015-06-26 17:03:40 -0300

    Updated the folder for tests

--- Tests/generateGraphs.sh
+++ Tests/generateGraphs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SIZES="500 512 1022 1024 1026 1512 2046 2048"
+#SIZES="50 51 102 102 102 151 204 204"
+SIZES="500 512 1022 1024 1026 2046 2048"
 
 # Remove previous test files
 echo "Removing previous test files..."
@@ -39,6 +40,6 @@ echo "plot \"out.dat\" with lines" >> showgraph.txt
 
 gnuplot --persist showgraph.txt
 
-#rm -f out.data
-#rm -f solution.txt
-#rm -f showgraph.txt
+# Remove files from the pdeSolver execution
+rm -f solution.txt
+rm -f out.data
---
+++ Tests/old_gs/mem/generate.plot
@@ -0,0 +1,3 @@
+set title "3D surface from a grid (matrix) of Z values"
+set datafile separator "|
+plot "grafico_gs_o0_mem.dat" with lines title "-O0", "grafico_gs_o3_mem.dat" with lines title "-O3", 

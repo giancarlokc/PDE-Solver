80382cdd8159b762ee898f0be2c8fcaf54b900a3
RENAN DOMINGOS MERLIN GRECA <rdmg11@inf.ufpr.br>
2015-06-30 16:43:44 -0300

    graficos graficos

--- likpde.sh
+++ likpde.sh
@@ -1,5 +1,5 @@
 # /bin/bash!
 
-echo "Rodando likwid com $1 nx=ny=$2"
+echo "likwid-perfctr -m -C 1 -g FLOPS_DP ./pdeSolver -nx $1 -ny $1 -i 10"
 
-likwid-perfctr -C 1 -g $1 ./pdeSolver -nx $2 -ny $2 -i 10
+likwid-perfctr -m -C 1 -g FLOPS_DP ./pdeSolver -nx $1 -ny $1 -i 10
